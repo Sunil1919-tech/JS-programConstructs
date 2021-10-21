c7c7935084bd9646bc3700d8807137d300cdfbf2
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-10-21 15:37:28 +0530

    Added:sum of Five Two digit Random Numbers

--- Sequence/Sequence3.js
+++ Sequence/Sequence3.js
@@ -1,8 +1,15 @@
-//Add two Random Dice Number and Print the Result 
+//Adding five two digit random values
 
-let diceNumber1 = Math.floor(((Math.random() * 10) % 6) + 1);
-console.log("first dice Value is " +diceNumber1);
-let diceNumber2 = Math.floor(((Math.random() * 10) % 6) + 1);
-console.log("The Second Dice Number is " +diceNumber2);
-let diceSum = diceNumber1 + diceNumber2;
-console.log("The sum of Two dice numbers are: " + diceSum);
+let diceNumber1 = Math.floor(((Math.random() * 100)) + 10);
+console.log("first dice Value is " + diceNumber1);
+let diceNumber2 = Math.floor(((Math.random() * 100)) + 10);
+console.log("The Second Dice Number is " + diceNumber2);
+let diceNumber3 = Math.floor(((Math.random() * 100)) + 10);
+console.log("The third Dice Number is " + diceNumber3);
+let diceNumber4 = Math.floor(((Math.random() * 100)) + 10);
+console.log("The Second Dice Number is " + diceNumber4);
+let diceNumber5 = Math.floor(((Math.random() * 100) ) + 10);
+console.log("The Second Dice Number is " + diceNumber5);
+
+let diceSum = diceNumber1 + diceNumber2+diceNumber3+diceNumber4+diceNumber5;
+console.log("The sum of Five dice numbers are: " + diceSum);
